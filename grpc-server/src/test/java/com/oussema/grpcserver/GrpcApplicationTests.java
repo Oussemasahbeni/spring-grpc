f0c53038f2c58b4f643b5bb3e3b05ab8febf740f
oussema sahbeni <oussemasahbeni300@gmail.com>
2025-05-17 13:53:37 +0100

    initial commit

---
+++ grpc-server/src/test/java/com/oussema/grpcserver/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussema.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
